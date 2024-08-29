1b26ae234d37a17ee02957df351392c19c58d8f0
floydw1234 <floydw1234@gmail.com>
2024-08-28 18:48:56 -0600

    fixed function

--- detect.py
+++ detect.py
@@ -30,12 +30,12 @@ parser.add_argument('--image')
 
 args=parser.parse_args()
 
-faceProto="opencv_face_detector.pbtxt"
-faceModel="opencv_face_detector_uint8.pb"
-ageProto="age_deploy.prototxt"
-ageModel="age_net.caffemodel"
-genderProto="gender_deploy.prototxt"
-genderModel="gender_net.caffemodel"
+faceProto="GADetect/opencv_face_detector.pbtxt"
+faceModel="GADetect/opencv_face_detector_uint8.pb"
+ageProto="GADetect/age_deploy.prototxt"
+ageModel="GADetect/age_net.caffemodel"
+genderProto="GADetect/gender_deploy.prototxt"
+genderModel="GADetect/gender_net.caffemodel"
 
 MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
 ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
@@ -45,33 +45,19 @@ faceNet=cv2.dnn.readNet(faceModel,faceProto)
 ageNet=cv2.dnn.readNet(ageModel,ageProto)
 genderNet=cv2.dnn.readNet(genderModel,genderProto)
 
-video=cv2.VideoCapture(args.image if args.image else 0)
-padding=20
-while cv2.waitKey(1)<0 :
-    hasFrame,frame=video.read()
-    if not hasFrame:
-        cv2.waitKey()
-        break
-    
-    resultImg,faceBoxes=highlightFace(faceNet,frame)
-    if not faceBoxes:
-        print("No face detected")
+def get_gender_age(img_path):
 
-    for faceBox in faceBoxes:
-        face=frame[max(0,faceBox[1]-padding):
-                   min(faceBox[3]+padding,frame.shape[0]-1),max(0,faceBox[0]-padding)
-                   :min(faceBox[2]+padding, frame.shape[1]-1)]
+    img = cv2.imread(img_path)
 
-        blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
-        genderNet.setInput(blob)
-        genderPreds=genderNet.forward()
-        gender=genderList[genderPreds[0].argmax()]
-        print(f'Gender: {gender}')
+    blob=cv2.dnn.blobFromImage(img, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+    genderNet.setInput(blob)
+    genderPreds=genderNet.forward()
+    gender=genderList[genderPreds[0].argmax()]
+    print(f'Gender: {gender}')
 
-        ageNet.setInput(blob)
-        agePreds=ageNet.forward()
-        age=ageList[agePreds[0].argmax()]
-        print(f'Age: {age[1:-1]} years')
+    ageNet.setInput(blob)
+    agePreds=ageNet.forward()
+    age=ageList[agePreds[0].argmax()]
+    print(f'Age: {age[1:-1]} years')
 
-        cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
-        cv2.imshow("Detecting age and gender", resultImg)
+    return {"gender":gender,"age":age}
